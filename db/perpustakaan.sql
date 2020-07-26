80f4350ac986938badaaf6154e2f01571d787db1
riama-kristallia <riamkrist@gmail.com>
2020-07-27 06:22:50 +0700

    update final

--- db/perpustakaan.sql
+++ db/perpustakaan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 26, 2020 at 01:34 PM
+-- Generation Time: Jul 27, 2020 at 01:22 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.0
 
@@ -43,8 +43,10 @@ CREATE TABLE `buku` (
 --
 
 INSERT INTO `buku` (`id_buku`, `judul`, `pengarang`, `penerbit`, `kategori`, `deskripsi`, `stok`) VALUES
-('B0001', 'Etika Kedokteran dan Hukum Keehatan Edisi 3', 'Prof. M. Jusuf Hanafiah, SpOG, Dr. Amri Amir, Spf', 'Buku Kedokteran ', 'Ilmu Kesehatan', '\n\n   Deskripsi', 60),
-('B0002', 'Dasar-Dasar Matematika Ekonomi Edisi Keempta Jilid 1', 'Alpha C. Chiang, KEvin Wainwright', 'Erlangga', 'Ilmu Sains dan Teknologi', 'Dasar-dasar matematika ekonomi', 50);
+('B0001', ' SEI CERT C Coding Standard', 'Carneige Mellon University', 'Carneige Mellon University', 'Ilmu Sains dan Teknologi', 'Standar Coding menggunakan bahasa C', 3),
+('B0002', 'SEI Seires in Software Engineering', 'Robert C. Secord', 'Pearson Education', 'Ilmu Kesehatan', 'Keamann Pemrogarman C', 10),
+('B0003', 'Research Methodology', 'C.R. Kothari', 'New Age International', 'Ilmu Sains dan Teknologi', 'Metodologi penelitian', 17),
+('B0004', 'Social Engineering', 'Christopher Hadnagy', 'John Willey & Sons', 'Ilmu Sosial dan Humaniora', 'rekayasa psikologis (social enginnering) melalui sudut pandang', 15);
 
 -- --------------------------------------------------------
 
@@ -53,6 +55,7 @@ INSERT INTO `buku` (`id_buku`, `judul`, `pengarang`, `penerbit`, `kategori`, `de
 --
 
 CREATE TABLE `login` (
+  `id` int(10) NOT NULL,
   `username` varchar(20) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -63,10 +66,11 @@ CREATE TABLE `login` (
 -- Dumping data for table `login`
 --
 
-INSERT INTO `login` (`username`, `nama`, `password`, `salt`) VALUES
-('arija', 'Muhammad Jalaludin Arija', '5bf87154fdba3b22f22aa484e9cc81464dd8cdfe5d9f2fdb302a68f3ab351b1f', '1aadc2bfb8514c6d91e2c62effa7fda3'),
-('riama', 'Riama Kristallia', 'c097814392faa26a8548882b76a571ce665de053c445cb98ce4f465b5f601940', '74642888fcc5418eb11369b66cb1aabb'),
-('nilam', 'Nilam Qolbi', '25ac027139fedd4f7ef153e93a798b139bde0dba8cd95d466d82191014e05677', '2dd4a04fc8ff470989ded0532454364c');
+INSERT INTO `login` (`id`, `username`, `nama`, `password`, `salt`) VALUES
+(1, 'arija', 'Muhammad Jalaludin Arija', '5bf87154fdba3b22f22aa484e9cc81464dd8cdfe5d9f2fdb302a68f3ab351b1f', '1aadc2bfb8514c6d91e2c62effa7fda3'),
+(2, 'riama', 'Riama Kristallia', 'c097814392faa26a8548882b76a571ce665de053c445cb98ce4f465b5f601940', '74642888fcc5418eb11369b66cb1aabb'),
+(3, 'nilam', 'Nilam Qolbi', '25ac027139fedd4f7ef153e93a798b139bde0dba8cd95d466d82191014e05677', '2dd4a04fc8ff470989ded0532454364c'),
+(6, 'admin', 'Administrator', '', '');
 
 -- --------------------------------------------------------
 
@@ -101,7 +105,7 @@ CREATE TABLE `pinjaman` (
   `npm` varchar(30) NOT NULL,
   `nama` varchar(30) NOT NULL,
   `id_buku` varchar(30) NOT NULL,
-  `judul` varchar(30) NOT NULL,
+  `judul` varchar(100) NOT NULL,
   `jumlah` int(30) NOT NULL,
   `tgl_pinjam` varchar(30) NOT NULL,
   `tgl_balik` varchar(30) NOT NULL,
@@ -114,9 +118,9 @@ CREATE TABLE `pinjaman` (
 --
 
 INSERT INTO `pinjaman` (`id_pinjam`, `npm`, `nama`, `id_buku`, `judul`, `jumlah`, `tgl_pinjam`, `tgl_balik`, `denda`, `status`) VALUES
-('P0001', '065116299', 'Bayu Junis Pribadi', 'B0003', 'Depok', 24, '06 February 2018', '08 February 2018', 0, 'Sudah Kembali'),
-('P0002', '1716101348', 'Nilam', 'B0002', 'rahasia ketamvanan uyab', 1, '21 July 2020', '28 July 2020', 0, 'Sudah Kembali'),
-('P0003', '1716101348', 'Nilam', 'B0001', 'Ketamvanan Bayu', 1, '21 July 2020', '22 July 2020', 5000, 'Sudah Kembali');
+('P0001', '1716101354', 'Riama Kristallia', 'B0001', ' SEI CERT C Coding Standard', 2, '22 July 2020', '25 July 2020', 0, 'Belum Kembali'),
+('P0002', '1716101348', 'Nilam Qolbi', 'B0003', 'Research Methodology', 3, '23 July 2020', '26 July 2020', 0, 'Belum Kembali'),
+('P0003', '1716101337', 'Muhammad Jalaluddin Arija', 'B0004', 'Social Engineering', 5, '23 July 2020', '26 July 2020', 0, 'Belum Kembali');
 
 --
 -- Indexes for dumped tables
@@ -129,6 +133,12 @@ ALTER TABLE `buku`
   ADD PRIMARY KEY (`id_buku`);
 
 --
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
@@ -139,6 +149,16 @@ ALTER TABLE `mahasiswa`
 --
 ALTER TABLE `pinjaman`
   ADD PRIMARY KEY (`id_pinjam`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
